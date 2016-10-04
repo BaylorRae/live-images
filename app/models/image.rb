e39201f305303d3a1262597a915451fbb4828fa3
Baylor Rae <baylor@thecodedeli.com>
2016-10-03 19:08:03 -0500

    add method for getting latest image

--- app/models/image.rb
+++ app/models/image.rb
@@ -3,4 +3,8 @@ class Image < ApplicationRecord
 
   has_attached_file :image, styles: { thumb: "400x300>", }
   validates_attachment_content_type :image, content_type: ["image/jpg", "image/jpeg", "image/png"]
+
+  def self.latest
+    order(created_at: :desc).first
+  end
 end
